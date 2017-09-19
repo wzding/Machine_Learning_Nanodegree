b32c7e8d511b4e915eb57f282272abc3de60502b
Wenzhe Ding <wding@elementum.com>
2017-09-19 09:33:11 -0700

    add brackets to print function

--- titanic_survival_exploration/visuals.py
+++ titanic_survival_exploration/visuals.py
@@ -20,18 +20,18 @@ def filter_data(data, condition):
     Conditions should be a list of strings of the following format:
       '<field> <op> <value>'
     where the following operations are valid: >, <, >=, <=, ==, !=
-    
+
     Example: ["Sex == 'male'", 'Age < 18']
     """
 
     field, op, value = condition.split(" ")
-    
+
     # convert value into number or strip excess quotes if string
     try:
         value = float(value)
     except:
         value = value.strip("\'\"")
-    
+
     # get booleans for filtering
     if op == ">":
         matches = data[field] > value
@@ -47,7 +47,7 @@ def filter_data(data, condition):
         matches = data[field] != value
     else: # catch invalid operation codes
         raise Exception("Invalid comparison operator. Only >, <, >=, <=, ==, != allowed.")
-    
+
     # filter data and outcomes
     data = data[matches].reset_index(drop = True)
     return data
@@ -57,37 +57,37 @@ def survival_stats(data, outcomes, key, filters = []):
     Print out selected statistics regarding survival, given a feature of
     interest and any number of filters (including no filters)
     """
-    
+
     # Check that the key exists
     if key not in data.columns.values :
-        print "'{}' is not a feature of the Titanic data. Did you spell something wrong?".format(key)
+        print("'{}' is not a feature of the Titanic data. Did you spell something wrong?".format(key))
         return False
 
     # Return the function before visualizing if 'Cabin' or 'Ticket'
     # is selected: too many unique categories to display
     if(key == 'Cabin' or key == 'PassengerId' or key == 'Ticket'):
-        print "'{}' has too many unique categories to display! Try a different feature.".format(key)
+        print("'{}' has too many unique categories to display! Try a different feature.".format(key))
         return False
 
     # Merge data and outcomes into single dataframe
     all_data = pd.concat([data, outcomes.to_frame()], axis = 1)
-    
+
     # Apply filters to data
     for condition in filters:
         all_data = filter_data(all_data, condition)
 
     # Create outcomes DataFrame
     all_data = all_data[[key, 'Survived']]
-    
+
     # Create plotting figure
     plt.figure(figsize=(8,6))
 
     # 'Numerical' features
     if(key == 'Age' or key == 'Fare'):
-        
+
         # Remove NaN values from Age data
         all_data = all_data[~np.isnan(all_data[key])]
-        
+
         # Divide the range of data into bins and count survival rates
         min_value = all_data[key].min()
         max_value = all_data[key].max()
@@ -98,7 +98,7 @@ def survival_stats(data, outcomes, key, filters = []):
             bins = np.arange(0, all_data['Fare'].max() + 20, 20)
         if(key == 'Age'):
             bins = np.arange(0, all_data['Age'].max() + 10, 10)
-        
+
         # Overlay each bin's survival rates
         nonsurv_vals = all_data[all_data['Survived'] == 0][key].reset_index(drop = True)
         surv_vals = all_data[all_data['Survived'] == 1][key].reset_index(drop = True)
@@ -106,14 +106,14 @@ def survival_stats(data, outcomes, key, filters = []):
                  color = 'red', label = 'Did not survive')
         plt.hist(surv_vals, bins = bins, alpha = 0.6,
                  color = 'green', label = 'Survived')
-    
+
         # Add legend to plot
         plt.xlim(0, bins.max())
         plt.legend(framealpha = 0.8)
-    
+
     # 'Categorical' features
     else:
-       
+
         # Set the various categories
         if(key == 'Pclass'):
             values = np.arange(1,4)
@@ -151,6 +151,5 @@ def survival_stats(data, outcomes, key, filters = []):
     # Report number of passengers with missing values
     if sum(pd.isnull(all_data[key])):
         nan_outcomes = all_data[pd.isnull(all_data[key])]['Survived']
-        print "Passengers with missing '{}' values: {} ({} survived, {} did not survive)".format( \
-              key, len(nan_outcomes), sum(nan_outcomes == 1), sum(nan_outcomes == 0))
-
+        print("Passengers with missing '{}' values: {} ({} survived, {} did not survive)".format( \
+              key, len(nan_outcomes), sum(nan_outcomes == 1), sum(nan_outcomes == 0)))
